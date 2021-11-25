31e3ec2fc235ad1403a6a1ac55ea5dc6fd38c59c
acharawan01 <630112418071@bru.ac.th>
2021-11-25 15:57:13 +0700

    LAB2

---
+++ LAB2/cs6-basic.js
@@ -0,0 +1,31 @@
+var name = "";
+sname = "vong";
+
+rname = "may";
+console.log(rname);
+
+{
+    let rname = "john";
+    console.log(rname); 
+}
+console.log(rname);
+
+{
+    const number = 10;
+//    number = 5;
+}
+
+//traditional function
+function add(a, b){
+  let c = a + b;  
+return c;
+}
+
+const addES5 = function (a, b){
+    return a + b;
+};
+
+const addES6 = (a, b) => a + b;
+
+const result = addES6(5, 8);
+console.log(result);
